af18153e63a96c5e4461cfd5a811a883275876d6
Vlad Shlosberg <vshlos@gmail.com>
2018-04-16 16:12:23 -0700

    auto increment build number

--- tools/create-package.js
+++ tools/create-package.js
@@ -3,13 +3,12 @@ import path from 'path';
 
 const oldPackage = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.json")));
 
-const newPackage = {
-    "name": oldPackage.name,
-    "version": oldPackage.version,
-    "private": oldPackage.private,
-    "dependencies": oldPackage.dependencies,
-    "main": "src/index.js"
-};
+const newPackage = {};
+["name", "version", "private", "dependencies", "repository", "bugs", "main"].forEach(key => {
+    newPackage[key] = oldPackage[key];
+});
 
+const split = newPackage.version.split(/\./g);
+newPackage.version = `${split[0]}.${split[1]}.${(process.env.CIRCLE_BUILD_NUM || split[2])}`;
 
-fs.writeFileSync(path.resolve(__dirname, "../build/injects/package.json"), JSON.stringify(newPackage, 0, 4));
+fs.writeFileSync(path.resolve(__dirname, "../build/native-injects/package.json"), JSON.stringify(newPackage, 0, 4));
