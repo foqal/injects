ddb1d4df9a28814e7a0460497c2d14c4336b30e4
Vlad Shlosberg <vshlos@gmail.com>
2018-04-16 22:49:12 -0700

    license file and its added to the build

--- tools/create-package.js
+++ tools/create-package.js
@@ -4,11 +4,8 @@ import path from 'path';
 const oldPackage = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.json")));
 
 const newPackage = {};
-["name", "version", "private", "dependencies", "repository", "bugs", "main"].forEach(key => {
+["name", "version", "private", "dependencies", "repository", "bugs", "main", "files"].forEach(key => {
     newPackage[key] = oldPackage[key];
 });
-
-//const split = newPackage.version.split(/\./g);
-//newPackage.version = `${split[0]}.${split[1]}.${(process.env.CIRCLE_BUILD_NUM || split[2])}`;
 
 fs.writeFileSync(path.resolve(__dirname, "../build/native-injects/package.json"), JSON.stringify(newPackage, 0, 4));
